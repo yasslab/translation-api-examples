e8dfd02a9649055eb617997e768f4b2172867523
Seiei Miyagi <hanachin@gmail.com>
2015-06-12 18:16:55 +0900

    Update example

--- gengo/step2-basic_post_call.rb
+++ gengo/step2-basic_post_call.rb
@@ -7,10 +7,14 @@ gengo = Gengo::API.new(
   sandbox:     true,
 )
 
+body_src=<<SRC
+We’ll develop the sample app using a combination of mockups, test-driven development (TDD), and integration tests. We’ll get started in Chapter 3 by creating static pages and then add a little dynamic content.
+SRC
+
 job1 = {
     type:         'text',
     slug:         'job test 1',
-    body_src:     'one two three four',
+    body_src:     body_src,
     lc_src:       'en',
     lc_tgt:       'ja',
     tier:         'standard',
@@ -20,11 +24,11 @@ job1 = {
 job2 = {
     type:         'text',
     slug:         'job test 2',
-    body_src:     'five six seven eight',
+    body_src:     body_src,
     lc_src:       'en',
     lc_tgt:       'ja',
-    tier:         'standard',
+    tier:         'pro',
     comment:      'This one has a comment',
 }
 
-puts JSON.dump(gengo.postTranslationJobs(jobs: {job1: job1, job2: job2}))
+puts JSON.dump(gengo.postTranslationJobs(jobs: {foo: job1, bar: job2}))
